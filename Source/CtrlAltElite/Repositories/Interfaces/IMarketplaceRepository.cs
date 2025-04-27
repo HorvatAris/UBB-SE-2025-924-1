4cddad7d414e15fce189ef8121415ff88b00005b
iliesariana <ariana.ilies@stud.ubbcluj.ro>
2025-04-27 21:39:39 +0300

    Update IMarketplaceRepository.cs

--- Source/CtrlAltElite/Repositories/Interfaces/IMarketplaceRepository.cs
+++ Source/CtrlAltElite/Repositories/Interfaces/IMarketplaceRepository.cs
@@ -6,7 +6,28 @@ using System.Threading.Tasks;
 
 namespace CtrlAltElite.Repositories.Interfaces
 {
-    class IMarketplaceRepository
+    using SteamStore.Models;
+    using System;
+    using System.Collections.Generic;
+    using System.Threading.Tasks;
+
+    public interface IMarketplaceRepository
     {
+
+        Task<bool> BuyItemAsync(Item item, User currentUser);
+
+        Task<List<Item>> GetAllListedItemsAsync();
+
+        Task<List<Item>> GetListedItemsByGameAsync(Game game);
+
+        Task MakeItemListableAsync(Game game, Item item);
+
+        Task MakeItemNotListableAsync(Game game, Item item);
+
+        Task UpdateItemPriceAsync(Game game, Item item);
+
+        User? GetCurrentUser();
+
+        Task<List<User>> GetAllUsersAsync();
     }
 }
