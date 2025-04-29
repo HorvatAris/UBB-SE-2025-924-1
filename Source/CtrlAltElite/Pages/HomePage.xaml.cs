c96a7ba0aaa99a0456df7df1c9160cd12be7fc60
Nikole775 <nikole.labo@stud.ubbcluj.ro>
2025-04-29 21:29:05 +0300

    Move the logic from the HomePage View to the ModelView
    Feature Documentation: MVVM Refactoring - Home Page Logic Migration
    Feature Overview
    Feature Name: MVVM Architecture Implementation for Home Page
    Description: Migrated all UI logic and business rules from HomePage.xaml.cs to HomePageViewModel.cs to properly implement the MVVM pattern.
    
    Problem
    Original code had tight coupling between view and logic (violating MVVM principles)
    
    UI logic was mixed with presentation code in code-behind
    
    Made the code difficult to test and maintain
    
    Limited flexibility for UI changes
    
    Solution
    Identified all business logic in HomePage.xaml.cs
    
    Created corresponding properties and commands in HomePageViewModel
    
    Implemented INotifyPropertyChanged for data binding
    
    Established proper data bindings in XAML
    
    Maintained all existing functionality while improving architecture
    
    Key Components Migrated:
    
    Game data loading and display logic
    
    Search functionality
    
    Filter system (rating, price range, tags)
    
    Navigation commands
    
    UI state management
    
    Added a PriceToString convertor in Utils
    
    Key Files Affected
    Pages/HomePage.xaml (Updated bindings)
    
    Pages/HomePage.xaml.cs (Stripped of business logic)
    
    ViewModels/HomePageViewModel.cs (New viewmodel implementation)
    
    Utils/Converters.cs (new PriceToStringConverter converter class added)

--- Source/CtrlAltElite/Pages/HomePage.xaml.cs
+++ Source/CtrlAltElite/Pages/HomePage.xaml.cs
@@ -45,13 +45,7 @@ namespace SteamStore.Pages
 
         private void SearchBox_TextChanged(object searchBox, TextChangedEventArgs textChangedEventArgument)
         {
-            string user_input = this.SearchBox.Text;
-            if (this.DataContext is HomePageViewModel viewModel)
-            {
-                viewModel.SearchGames(user_input);
-            }
-
-            this.GameListView.UpdateLayout();
+            this.HomePageViewModel.SearchGames(this.SearchBox.Text);
         }
 
         private void FilterButton_Click(object filterButton, RoutedEventArgs filterClickEventArgument)
@@ -62,38 +56,30 @@ namespace SteamStore.Pages
         private void ApplyFilters_Click(object applyFiltersButton, RoutedEventArgs applyFiltersArgument)
         {
             // You can access the filter values from PopupRatingSlider, MinPriceSlider, MaxPriceSlider here.
-            int ratingFilter = (int)this.PopupRatingSlider.Value;
-            int minimumPrice = (int)this.MinPriceSlider.Value;
-            int maximumPrice = (int)this.MaxPriceSlider.Value;
-            var selectedTags = new List<string>();
+            this.HomePageViewModel.RatingFilter = (int)this.PopupRatingSlider.Value;
+            this.HomePageViewModel.MinPrice = (int)this.MinPriceSlider.Value;
+            this.HomePageViewModel.MaxPrice = (int)this.MaxPriceSlider.Value;
+
+            this.HomePageViewModel.SelectedTags.Clear();
             foreach (var item in this.TagListView.SelectedItems)
             {
                 if (item is Tag tag)
                 {
-                    selectedTags.Add(tag.Tag_name);
+                    this.HomePageViewModel.SelectedTags.Add(tag.Tag_name);
                 }
             }
 
-            if (this.DataContext is HomePageViewModel viewModel)
-            {
-                viewModel.FilterGames(ratingFilter, minimumPrice, maximumPrice, selectedTags.ToArray());
-            }
-
-            // Close the popup
+            this.HomePageViewModel.ApplyFilters();
             this.FilterPopup.IsOpen = false;
         }
 
         private void ResetFilters_Click(object resetFiltersButton, RoutedEventArgs resetFiltersClickEventArgument)
         {
-            this.PopupRatingSlider.Value = RATINGFILTERVALUE;
-            this.MinPriceSlider.Value = MINIMUMPRICEFILTERVALUE;
-            this.MaxPriceSlider.Value = MAXIMUMPRICEFILTERVALUE;
+            this.HomePageViewModel.ResetFilters();
+            this.PopupRatingSlider.Value = 0;
+            this.MinPriceSlider.Value = 0;
+            this.MaxPriceSlider.Value = 200;
             this.TagListView.SelectedItems.Clear();
-            if (this.DataContext is HomePageViewModel viewModel)
-            {
-                viewModel.LoadAllGames();
-            }
-
             this.FilterPopup.IsOpen = false;
         }
 
@@ -102,13 +88,7 @@ namespace SteamStore.Pages
         {
             if (gameListView is ListView listView && listView.SelectedItem is Game selectedGame)
             {
-                // Get the services from DataContext
-                if (this.DataContext is HomePageViewModel viewModel)
-                {
-                    viewModel.SwitchToGamePage(this.Parent, selectedGame);
-                }
-
-                // Clear selection
+                this.HomePageViewModel.NavigateToGamePage(this.Parent as Frame, selectedGame);
                 listView.SelectedItem = null;
             }
         }
